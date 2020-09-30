5b07e7f8fc140e65e0f3f134c80c897a6c2092ac
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-09-30 11:53:53 +0200

    refactor(timeseries): change function name

--- src/timeseries.py
+++ src/timeseries.py
@@ -11,31 +11,27 @@ def get_time_series(content_type: ServiceKey, args) -> TimeSeriesResponse:
     theme_profile = args.get(QueryParameter.THEME_PROFILE)
     organization_id = args.get(QueryParameter.ORGANIZATION_ID)
     if content_type == ServiceKey.DATA_SETS:
-        return get_dataset_time_series(org_path=orgpath,
-                                       theme=theme,
-                                       theme_profile=theme_profile,
-                                       organization_id=organization_id)
+        return get_time_series_response(report_type=content_type,
+                                        org_path=orgpath,
+                                        theme=theme,
+                                        theme_profile=theme_profile,
+                                        organization_id=organization_id,
+                                        series_field=f"{EsMappings.RECORD}.{JSON_RDF.dct.issued}.value")
     elif content_type in [ServiceKey.CONCEPTS, ServiceKey.INFO_MODELS]:
         return get_time_series_response(report_type=content_type,
                                         org_path=orgpath,
-                                        organization_id=organization_id)
+                                        organization_id=organization_id,
+                                        series_field=EsMappings.FIRST_HARVESTED)
     else:
         raise KeyError()
 
 
-def get_dataset_time_series(org_path=None, theme=None, theme_profile=None, organization_id=None) -> TimeSeriesResponse:
-    es_time_series = elasticsearch_get_time_series(report_type=ServiceKey.DATA_SETS,
+def get_time_series_response(report_type=None, org_path=None, theme=None, theme_profile=None, organization_id=None,
+                             series_field=None) -> TimeSeriesResponse:
+    es_time_series = elasticsearch_get_time_series(report_type=report_type,
                                                    org_path=org_path,
                                                    theme=theme,
                                                    theme_profile=theme_profile,
                                                    organization_id=organization_id,
-                                                   series_field=f"{EsMappings.RECORD}.{JSON_RDF.dct.issued}.value")
-    return TimeSeriesResponse(es_time_series)
-
-
-def get_time_series_response(report_type=None, org_path=None, organization_id=None) -> TimeSeriesResponse:
-    es_time_series = elasticsearch_get_time_series(report_type=report_type,
-                                                   org_path=org_path,
-                                                   organization_id=organization_id,
-                                                   series_field=EsMappings.FIRST_HARVESTED)
+                                                   series_field=series_field)
     return TimeSeriesResponse(es_time_series)
