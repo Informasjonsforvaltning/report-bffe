77c18958e3d1dfe75cd3c2a63bf42e149bdbb090
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-01-15 10:28:31 +0100

    fix(updates): update endpoint, parse date only if date exists

--- src/elasticsearch/__init__.py
+++ src/elasticsearch/__init__.py
@@ -11,18 +11,31 @@ es_client = Elasticsearch([ES_HOST + ":" + ES_PORT])
 
 def get_all_update_entries() -> List[dict]:
     updates = es_client.search(
+        size="100",
         index="updates",
-        body={"query": {"match_all": {}}, "sort": [{"end_time": {"order": "desc"}}]},
+        body={"query": {"match_all": {}}, "sort": [{"start_time": {"order": "desc"}}]},
     )
     update_list = [hit["_source"] for hit in updates["hits"]["hits"]]
     response = []
     for update in update_list:
-        end_time = datetime.strptime(update["end_time"], "%Y-%m-%dT%H:%M:%S.%f%z")
-        start_time = datetime.strptime(update["start_time"], "%Y-%m-%dT%H:%M:%S.%f%z")
+        end_time = (
+            datetime.strptime(update["end_time"], "%Y-%m-%dT%H:%M:%S.%f%z").strftime(
+                "%H:%M:%S | %d.%m.%Y"
+            )
+            if "end_time" in update
+            else ""
+        )
+        start_time = (
+            datetime.strptime(update["start_time"], "%Y-%m-%dT%H:%M:%S.%f%z").strftime(
+                "%H:%M:%S | %d.%m.%Y"
+            )
+            if "start_time" in update
+            else ""
+        )
         response.append(
             {
-                "start_time": start_time.strftime("%H:%M:%S | %d.%m.%Y"),
-                "end_time": end_time.strftime("%H:%M:%S | %d.%m.%Y"),
+                "start_time": start_time,
+                "end_time": end_time,
                 "status": update["status"],
             }
         )
