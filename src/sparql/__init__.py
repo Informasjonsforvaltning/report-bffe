7c732746d2a5a79dc5784eb078a0a571d63613e0
Alexander Zakharov <alzak@envisage.no>
2021-03-10 10:52:38 +0100

    fix: remove sparql query builder

--- src/sparql/__init__.py
+++ src/sparql/__init__.py
@@ -1,18 +1,52 @@
-from src.sparql.builder import encode_for_sparql
-from src.sparql.queries import (
-    build_dataservice_publisher_query,
-    build_dataservice_query,
-    build_dataset_publisher_query,
-)
-
-
 def get_dataset_publisher_query():
-    return encode_for_sparql(build_dataset_publisher_query())
+    return """
+        PREFIX dct: <http://purl.org/dc/terms/>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        SELECT ?name ?publisher ?sameAs
+        FROM <https://datasets.fellesdatakatalog.digdir.no>
+        WHERE {{
+            ?publisher a foaf:Agent .
+            ?publisher foaf:name ?name .
+            OPTIONAL {{ ?publisher owl:sameAs ?sameAs }}
+        }}
+        GROUP BY ?name ?publisher ?sameAs
+    """
 
 
 def get_dataservice_query():
-    return encode_for_sparql(build_dataservice_query())
+    return """
+        PREFIX dct: <http://purl.org/dc/terms/>
+        PREFIX dcat: <http://www.w3.org/ns/dcat#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        SELECT ?record ?publisher ?issued ?sameAs ?mediaType
+        FROM <https://dataservices.fellesdatakatalog.digdir.no>
+        WHERE {{
+            ?catalog a dcat:Catalog .
+            ?catalog dct:publisher ?publisher .
+            ?catalog dcat:service ?service .
+            ?record foaf:primaryTopic ?service .
+            ?record dct:issued ?issued .
+            OPTIONAL {{
+                ?publisher owl:sameAs ?sameAs .
+                ?service dcat:mediaType ?mediaType
+            }}
+        }}
+    """
 
 
 def get_dataservice_publisher_query():
-    return encode_for_sparql(build_dataservice_publisher_query())
+    return """
+        PREFIX dct: <http://purl.org/dc/terms/>
+        PREFIX dcat: <http://www.w3.org/ns/dcat#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        SELECT ?publisher ?sameAs
+        FROM <https://dataservices.fellesdatakatalog.digdir.no>
+        WHERE {{
+            ?publisher a foaf:Agent .
+            OPTIONAL {{ ?publisher owl:sameAs ?sameAs }}
+        }}
+        GROUP BY ?publisher ?sameAs
+    """
