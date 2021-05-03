7b15e3d42fc40a69da3c298378e988dd7b22fa9b
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-05-03 12:30:28 +0200

    chore: moved from <portal>/sparql to sparql.<portal>

--- src/service_requests.py
+++ src/service_requests.py
@@ -193,7 +193,7 @@ async def fetch_catalog_from_dataset_harvester() -> dict:
 
 async def fetch_publishers_from_dataset_harvester() -> dict:
     publisher_query = urllib.parse.quote_plus(get_dataset_publisher_query())
-    url = f"{service_urls.get(ServiceKey.FDK_BASE)}/sparql?query={publisher_query}"
+    url = f"{service_urls.get(ServiceKey.SPARQL_BASE)}?query={publisher_query}"
     async with AsyncClient() as session:
         try:
             response = await session.get(
@@ -275,7 +275,7 @@ async def fetch_all_concepts():
 # dataservices
 async def fetch_dataservices() -> dict:
     dataservice_query = urllib.parse.quote_plus(get_dataservice_query())
-    url = f"{service_urls.get(ServiceKey.FDK_BASE)}/sparql?query={dataservice_query}"
+    url = f"{service_urls.get(ServiceKey.SPARQL_BASE)}?query={dataservice_query}"
     async with AsyncClient() as session:
         try:
             response = await session.get(
@@ -296,7 +296,7 @@ async def fetch_dataservices() -> dict:
 
 async def fetch_publishers_from_dataservice() -> dict:
     publisher_query = urllib.parse.quote_plus(get_dataservice_publisher_query())
-    url = f"{service_urls.get(ServiceKey.FDK_BASE)}/sparql?query={publisher_query}"
+    url = f"{service_urls.get(ServiceKey.SPARQL_BASE)}?query={publisher_query}"
     async with AsyncClient() as session:
         try:
             response = await session.get(
