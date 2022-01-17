0b8fa459c511e61f376cd640eb63d8bc94fed222
NilsOveTen <github.nils.ove@tendenes.com>
2022-01-17 14:17:25 +0100

    chore: get orgPath & orgId for concepts & data services from SPARQL

--- src/fdk_reports_bff/elasticsearch/concepts.py
+++ src/fdk_reports_bff/elasticsearch/concepts.py
@@ -3,17 +3,15 @@ import logging
 import traceback
 from typing import List
 
-from fdk_reports_bff.elasticsearch.queries import CONCEPT_AGGREGATION_FIELDS
+from fdk_reports_bff.elasticsearch.queries import (
+    CONCEPT_AGGREGATION_FIELDS,
+    EsMappings,
+)
 from fdk_reports_bff.elasticsearch.utils import (
-    add_org_paths_to_document,
     elasticsearch_ingest,
-    get_all_organizations_with_publisher,
     get_unique_records,
 )
-from fdk_reports_bff.service.service_requests import (
-    fetch_all_concepts,
-    fetch_concept_publishers,
-)
+from fdk_reports_bff.service.service_requests import fetch_all_concepts
 from fdk_reports_bff.service.utils import FetchFromServiceException, ServiceKey
 
 
@@ -24,12 +22,10 @@ def insert_concepts(success_status: str, failed_status: str) -> str:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
     try:
-        concept_tasks = asyncio.gather(fetch_all_concepts(), fetch_concept_publishers())
-        concepts, publishers = loop.run_until_complete(concept_tasks)
+        concept_tasks = asyncio.gather(fetch_all_concepts())
+        concepts = loop.run_until_complete(concept_tasks)[0]
 
-        prepared_docs = loop.run_until_complete(
-            prepare_documents(documents=concepts, publishers=publishers)
-        )
+        prepared_docs = loop.run_until_complete(prepare_documents(documents=concepts))
 
         elasticsearch_ingest(index_key=ServiceKey.CONCEPTS, documents=prepared_docs)
         return success_status
@@ -39,17 +35,10 @@ def insert_concepts(success_status: str, failed_status: str) -> str:
         return failed_status
 
 
-async def prepare_documents(documents: List[dict], publishers: dict) -> List[dict]:
+async def prepare_documents(documents: List[dict]) -> List[dict]:
     unique_record_items = get_unique_records(documents)
 
-    await get_all_organizations_with_publisher(publishers)
-    concepts_with_fdk_portal_paths = await asyncio.gather(
-        *[add_org_paths_to_document(rdf_values=entry) for entry in unique_record_items]
-    )
-
-    return [
-        reduce_concept(concept=concept) for concept in concepts_with_fdk_portal_paths
-    ]
+    return [reduce_concept(concept=concept) for concept in unique_record_items]
 
 
 def reduce_concept(concept: dict) -> dict:
@@ -58,4 +47,6 @@ def reduce_concept(concept: dict) -> dict:
         key = items[0]
         if key not in CONCEPT_AGGREGATION_FIELDS:
             reduced_dict.pop(key)
+        elif key in EsMappings.ORG_PATH or key in EsMappings.ORGANIZATION_ID:
+            reduced_dict[key] = concept[key]["value"]
     return reduced_dict
--- src/fdk_reports_bff/sparql/__init__.py
+++ src/fdk_reports_bff/sparql/__init__.py
@@ -1,12 +1,4 @@
-from fdk_reports_bff.sparql.concept import (
-    get_concept_publishers_query,
-    get_concepts_query,
-)
-from fdk_reports_bff.sparql.data_service import (
-    get_dataservice_publisher_query,
-    get_dataservice_query,
-)
+from fdk_reports_bff.sparql.concept import get_concepts_query
+from fdk_reports_bff.sparql.data_service import get_dataservice_query
 from fdk_reports_bff.sparql.dataset import get_dataset_publisher_query
-from fdk_reports_bff.sparql.information_model import (
-    get_info_models_query,
-)
+from fdk_reports_bff.sparql.information_model import get_info_models_query
