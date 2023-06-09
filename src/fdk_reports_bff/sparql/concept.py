d9441c76de1fcdf430c2981532c94b31dfc91e72
NilsOveTen <github.nils.ove@tendenes.com>
2023-06-09 09:31:35 +0200

    fix: rm bad optionals from sparql requests

--- src/fdk_reports_bff/sparql/concept.py
+++ src/fdk_reports_bff/sparql/concept.py
@@ -1,35 +1,38 @@
 def get_concepts_query() -> str:
     return """
-        PREFIX dct: <http://purl.org/dc/terms/>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        PREFIX br: <https://raw.githubusercontent.com/Informasjonsforvaltning/organization-catalog/main/src/main/resources/ontology/organization-catalog.owl#>
-        SELECT DISTINCT ?record ?firstHarvested ?publisher ?orgId ?orgPath
-        FROM <https://concepts.fellesdatakatalog.digdir.no>
-        WHERE {{
-            ?concept a skos:Concept .
-            ?record foaf:primaryTopic ?concept .
-            ?record dct:issued ?firstHarvested .
-            ?collection skos:member ?concept .
-            OPTIONAL {{ ?concept dct:publisher ?publisher . }}
-            OPTIONAL {{ ?publisher dct:identifier ?orgId . }}
-            OPTIONAL {{ ?publisher br:orgPath ?orgPath . }}
-        }}
-    """
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX br: <https://raw.githubusercontent.com/Informasjonsforvaltning/organization-catalog/main/src/main/resources/ontology/organization-catalog.owl#>
+SELECT DISTINCT ?record ?firstHarvested ?publisher ?orgId ?orgPath
+FROM <https://concepts.fellesdatakatalog.digdir.no>
+WHERE {
+  ?concept a skos:Concept .
+  ?record foaf:primaryTopic ?concept .
+  ?record dct:issued ?firstHarvested .
+  ?collection skos:member ?concept .
+  OPTIONAL {
+    ?concept dct:publisher ?publisher .
+    ?publisher dct:identifier ?orgId .
+    ?publisher br:orgPath ?orgPath .
+  }
+}"""
 
 
 def concept_timeseries_datapoint_query() -> str:
     return """
-        PREFIX dcat: <http://www.w3.org/ns/dcat#>
-        PREFIX dct: <http://purl.org/dc/terms/>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        PREFIX br: <https://raw.githubusercontent.com/Informasjonsforvaltning/organization-catalog/main/src/main/resources/ontology/organization-catalog.owl#>
-        SELECT DISTINCT ?concept ?orgPath
-        WHERE {
-            ?concept a skos:Concept .
-            ?record foaf:primaryTopic ?concept .
-            ?record a dcat:CatalogRecord .
-            OPTIONAL { ?concept dct:publisher ?publisher . }
-            OPTIONAL { ?publisher br:orgPath ?orgPath . }
-        }"""
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX br: <https://raw.githubusercontent.com/Informasjonsforvaltning/organization-catalog/main/src/main/resources/ontology/organization-catalog.owl#>
+SELECT DISTINCT ?concept ?orgPath
+WHERE {
+  ?concept a skos:Concept .
+  ?record foaf:primaryTopic ?concept .
+  ?record a dcat:CatalogRecord .
+  OPTIONAL {
+    ?concept dct:publisher ?publisher .
+    ?publisher br:orgPath ?orgPath .
+  }
+}"""
--- src/fdk_reports_bff/sparql/data_service.py
+++ src/fdk_reports_bff/sparql/data_service.py
@@ -1,37 +1,40 @@
 def get_dataservice_query() -> str:
     return """
-        PREFIX dct: <http://purl.org/dc/terms/>
-        PREFIX dcat: <http://www.w3.org/ns/dcat#>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        PREFIX br: <https://raw.githubusercontent.com/Informasjonsforvaltning/organization-catalog/main/src/main/resources/ontology/organization-catalog.owl#>
-        SELECT ?record ?publisher ?firstHarvested ?mediaType ?format ?orgId ?orgPath
-        FROM <https://dataservices.fellesdatakatalog.digdir.no>
-        WHERE {{
-            ?service a dcat:DataService .
-            ?record foaf:primaryTopic ?service .
-            ?record a dcat:CatalogRecord .
-            ?record dct:issued ?firstHarvested .
-            OPTIONAL {{ ?service dct:publisher ?publisher . }}
-            OPTIONAL {{ ?publisher dct:identifier ?orgId . }}
-            OPTIONAL {{ ?publisher br:orgPath ?orgPath . }}
-            OPTIONAL {{ ?service dcat:mediaType ?mediaType . }}
-            OPTIONAL {{ ?service dct:format ?format . }}
-        }}
-    """
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX br: <https://raw.githubusercontent.com/Informasjonsforvaltning/organization-catalog/main/src/main/resources/ontology/organization-catalog.owl#>
+SELECT DISTINCT ?record ?publisher ?firstHarvested ?mediaType ?format ?orgId ?orgPath
+FROM <https://dataservices.fellesdatakatalog.digdir.no>
+WHERE {
+  ?service a dcat:DataService .
+  ?record foaf:primaryTopic ?service .
+  ?record a dcat:CatalogRecord .
+  ?record dct:issued ?firstHarvested .
+  OPTIONAL { ?service dcat:mediaType ?mediaType . }
+  OPTIONAL { ?service dct:format ?format . }
+  OPTIONAL {
+    ?service dct:publisher ?publisher .
+    ?publisher dct:identifier ?orgId .
+    ?publisher br:orgPath ?orgPath .
+  }
+}"""
 
 
 def dataservice_timeseries_datapoint_query() -> str:
     return """
-        PREFIX dct: <http://purl.org/dc/terms/>
-        PREFIX dcat: <http://www.w3.org/ns/dcat#>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        PREFIX br: <https://raw.githubusercontent.com/Informasjonsforvaltning/organization-catalog/main/src/main/resources/ontology/organization-catalog.owl#>
-        SELECT DISTINCT ?service ?orgPath
-        WHERE {
-            ?service a dcat:DataService .
-            ?record foaf:primaryTopic ?service .
-            ?record a dcat:CatalogRecord .
-            OPTIONAL {{ ?service dct:publisher ?publisher . }}
-            OPTIONAL {{ ?publisher br:orgPath ?orgPath . }}
-        }"""
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX br: <https://raw.githubusercontent.com/Informasjonsforvaltning/organization-catalog/main/src/main/resources/ontology/organization-catalog.owl#>
+SELECT DISTINCT ?service ?orgPath
+WHERE {
+  ?service a dcat:DataService .
+  ?record foaf:primaryTopic ?service .
+  ?record a dcat:CatalogRecord .
+  OPTIONAL {
+    ?service dct:publisher ?publisher .
+    ?publisher br:orgPath ?orgPath .
+  }
+}"""
