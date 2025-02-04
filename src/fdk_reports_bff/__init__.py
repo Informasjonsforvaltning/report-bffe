224dadff387c545f12f9b7c53966adeb3d429ca9
torleifg <torgravels@gmail.com>
2025-02-04 11:55:12 +0100

    Add custom cors setup

--- src/fdk_reports_bff/__init__.py
+++ src/fdk_reports_bff/__init__.py
@@ -17,7 +17,11 @@ def create_app(test_config: Any = None) -> Flask:
     load_dotenv(override=True)
     app = Flask(__name__, instance_relative_config=True)
 
-    CORS(app)
+    origins = os.getenv("CORS_ORIGIN_PATTERNS", "*").split(",")
+    origins = [origin.strip() for origin in origins]
+
+    CORS(app, origins=origins, allow_headers="*", methods=["GET", "POST"])
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile("config.py", silent=True)
